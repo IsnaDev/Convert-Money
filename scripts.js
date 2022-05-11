2eb761174249b1c765891752608957cfe7bfee65
Isnaíra Souza <ariansi_anjo@hotmail.com>
2022-05-11 16:02:04 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,64 @@
+const button = document.getElementById('convert-button') //click do button
+const select = document.getElementById("currency-select")
+
+const dolar = 5.95 //valor fixado
+const euro = 5.40
+const bitcoin = 153283
+
+const convertValues = () => { //função: converter do real para o dólar ou euro
+    const inputReais = document.getElementById('input-real').value //valor do input
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(inputReais)
+
+    if (select.value === "US$ Dólar americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            {
+                style: 'currency',
+                currency: 'USD'
+            }).format(inputReais / dolar)
+    }
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            {
+                style: 'currency',
+                currency: 'EUR'
+            }).format(inputReais / euro)
+    }
+
+    if (select.value === "Bitcoin") {
+        currencyValueText.innerHTML = (inputReais / bitcoin)
+    }
+}
+
+changeCurrency = () => { //verifica se o select é dólar ou euro e muda a imagem e o texto
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById("currency-img")
+
+    if (select.value === 'US$ Dólar americano') {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "./assets/estados-unidos.png"
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro.png"
+    }
+
+    if (select.value === 'Bitcoin') {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
+    }
+    
+    convertValues() //converte automático qdo muda o select
+}
+
+//ouvidor de eventos
+button.addEventListener('click', convertValues) //qdo button é clicado chama a função convertValues
+select.addEventListener('change', changeCurrency) //qdo select troca de valor chama a funçãochangeCurrency
